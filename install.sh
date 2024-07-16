6f8d9fe6dc86522a806c759756474fe672303df2
vemrog <vemrog@gmail.com>
2024-07-16 12:42:27 +0300

    new file:   install.s

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo pacman -S xorg xorg-xinit i3-gaps kitty ranger rofi firefox neovim polybar telegram-desktop nerd-fonts feh git zsh lightdm lightdm-gtk-greeter maim xclip dunst
+
+sudo systemctl enable lightdm
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -rf yay
+
+yay -S visual-studio-code-bin
+
+CONFIG_DIR="$HOME/.config"
+REPO_CONFIG_DIR="config"
+
+REPO_PICTURES_DIR="Pictures"
+HOME_PICTURES_DIR="$HOME/Pictures"
+
+mkdir -p "$HOME_PICTURES_DIR"
+
+cp "$REPO_PICTURES_DIR/"* "$HOME_PICTURES_DIR/"
+cp -r "$REPO_CONFIG_DIR/"* "$CONFIG_DIR/"
+
+chmod +x "$CONFIG_DIR/rofi/powermenu.sh"
+chmod +x "$CONFIG_DIR/polybar/launch.sh"
