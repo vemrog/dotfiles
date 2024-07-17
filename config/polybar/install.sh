1b18369da1fb86d22a8c1412991d96585ad1ec7f
vemrog <vemrog@gmail.com>
2024-07-17 09:57:28 +0300

    modified:   config/rofi/powermenu.sh

---
+++ config/polybar/install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Install necessary packages
+sudo pacman -S xorg xorg-xinit i3-gaps kitty ranger rofi neovim polybar nerd-fonts feh zsh lightdm lightdm-gtk-greeter maim xclip dunst ttf-fira-code
+
+# Enable LightDM to start on boot
+sudo systemctl enable lightdm
+
+# Install yay AUR helper
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -rf yay
+
+# Install Oh My Zsh without running the new shell
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+
+# Set up configuration and pictures directories
+CONFIG_DIR="$HOME/.config"
+REPO_CONFIG_DIR="config"
+REPO_PICTURES_DIR="images"
+HOME_PICTURES_DIR="$HOME/Pictures"
+
+# Create Pictures directory if it doesn't exist and copy images
+mkdir -p "$HOME_PICTURES_DIR"
+cp "$REPO_PICTURES_DIR/"* "$HOME_PICTURES_DIR/"
+
+# Restore the original .zshrc
+cp ".zshrc" "$HOME/"
+cp -r "$REPO_CONFIG_DIR/"* "$CONFIG_DIR/"
+
+# Make scripts executable
+chmod +x "$CONFIG_DIR/rofi/powermenu.sh"
+chmod +x "$CONFIG_DIR/polybar/launch.sh"
+
+# Ask user if they want to install additional packages
+read -p "Do you want to install additional packages? (y/n): " install_extra
+
+if [[ "$install_extra" == "y" || "$install_extra" == "Y" ]]; then
+    echo "Installing additional packages..."
+    # Add your additional packages here
+    sudo pacman -S telegram-desktop firefox blender clang obsidian
+    yay -S visual-studio-code-bin
+else
+    echo "Skipping additional packages installation."
+fi
--- config/rofi/powermenu.sh
+++ config/rofi/powermenu.sh
@@ -7,11 +7,7 @@ uptime="`uptime -p | sed -e 's/up //g'`"
 # Options
 shutdown='󰐥'
 reboot='󰜉'
-lock=''
-suspend='󰒲'
 logout='󰿅'
-yes='Yes'
-no='No'
 
 # Rofi CMD
 rofi_cmd() {
@@ -21,54 +17,27 @@ rofi_cmd() {
 		-theme ${dir}/${theme}.rasi
 }
 
-# Confirmation CMD
-confirm_cmd() {
-	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 250px;}' \
-		-theme-str 'mainbox {children: [ "message", "listview" ];}' \
-		-theme-str 'listview {columns: 2; lines: 1;}' \
-		-theme-str 'element-text {horizontal-align: 0.5;}' \
-		-theme-str 'textbox {horizontal-align: 0.5;}' \
-		-dmenu \
-		-p 'Confirmation' \
-		-mesg 'Are you Sure?' \
-		-theme ${dir}/${theme}.rasi
-}
-
-# Ask for confirmation
-confirm_exit() {
-	echo -e "$yes\n$no" | confirm_cmd
-}
-
 # Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "$shutdown\n$reboot\n$logout\n$suspend\n$lock" | rofi_cmd
+	echo -e "$shutdown\n$reboot\n$logout" | rofi_cmd
 }
 
 # Execute Command
 run_cmd() {
-	selected="$(confirm_exit)"
-	if [[ "$selected" == "$yes" ]]; then
-		if [[ $1 == '--shutdown' ]]; then
-			systemctl poweroff
-		elif [[ $1 == '--reboot' ]]; then
-			systemctl reboot
-		elif [[ $1 == '--suspend' ]]; then
-			mpc -q pause
-			amixer set Master mute
-			systemctl suspend
-		elif [[ $1 == '--logout' ]]; then
-			if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
-				openbox --exit
-			elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
-				bspc quit
-			elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
-				i3-msg exit
-			elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
-				qdbus org.kde.ksmserver /KSMServer logout 0 0 0
-			fi
+	if [[ $1 == '--shutdown' ]]; then
+		systemctl poweroff
+	elif [[ $1 == '--reboot' ]]; then
+		systemctl reboot
+	elif [[ $1 == '--logout' ]]; then
+		if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
+			openbox --exit
+		elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
+			bspc quit
+		elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
+			i3-msg exit
+		elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
+			qdbus org.kde.ksmserver /KSMServer logout 0 0 0
 		fi
-	else
-		exit 0
 	fi
 }
 
@@ -80,16 +49,6 @@ case ${chosen} in
         ;;
     $reboot)
 		run_cmd --reboot
-        ;;
-    $lock)
-		if [[ -x '/usr/bin/betterlockscreen' ]]; then
-			betterlockscreen -l
-		elif [[ -x '/usr/bin/i3lock' ]]; then
-			i3lock
-		fi
-        ;;
-    $suspend)
-		run_cmd --suspend
         ;;
     $logout)
 		run_cmd --logout
