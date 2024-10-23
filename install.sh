2548b6c2b248345c6f99f415b99bc54e3819a6b4
vemrog <160019318+vemrog@users.noreply.github.com>
2024-10-23 09:23:51 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,35 +25,28 @@ sudo systemctl enable lightdm
 sudo systemctl enable bluetooth
 sudo systemctl enable NetworkManager.service
 
-# Define paths
-CONFIG_DIR="$HOME/.config"
-REPO_CONFIG_DIR="$REPO_DIR/config"
-REPO_PICTURES_DIR="$REPO_DIR/images"
-HOME_PICTURES_DIR="$HOME/Pictures"
-NVIM_CONFIG_DIR="$CONFIG_DIR/nvim"
-
 # Copy ranger default configuration
 ranger --copy-config=all
 
 # Create necessary directories
-mkdir -p "$HOME_PICTURES_DIR"
-mkdir -p "$NVIM_CONFIG_DIR"
+mkdir -p "$HOME/Pictures"
+mkdir -p "$HOME/.config/nvim"
 
 # Copy image files and config files
-cp "$REPO_PICTURES_DIR/"* "$HOME_PICTURES_DIR/"
-cp -r "$REPO_CONFIG_DIR/"* "$CONFIG_DIR/"
+cp "$REPO_DIR/images/"* "$HOME/Pictures/"
+cp -r "$REPO_DIR/config/"* "$HOME/.config/"
 
 # Copy i3 desktop entry for display managers
-sudo cp "$REPO_CONFIG_DIR/i3.desktop" "/usr/share/xsessions/i3.desktop"
+sudo cp "$REPO_DIR/config/i3.desktop" "/usr/share/xsessions/i3.desktop"
 
 # Copy Neovim configuration
-cp -r "$REPO_DIR/nvim/"* "$NVIM_CONFIG_DIR/"
+cp -r "$REPO_DIR/nvim/"* "$HOME/.config/nvim/"
 
 # Make scripts executable
-chmod +x "$CONFIG_DIR/polybar/launch.sh"
-chmod +x "$CONFIG_DIR/rofi/powermenu.sh"
-chmod +x "$CONFIG_DIR/rofi/wifi-menu.sh"
-chmod +x "$CONFIG_DIR/rofi//layout.sh"
+chmod +x "$HOME/.config/polybar/launch.sh"
+chmod +x "$HOME/.config/rofi/powermenu.sh"
+chmod +x "$HOME/.config/rofi/wifi-menu.sh"
+chmod +x "$HOME/.config/rofi/layout.sh"
 
 # Ask to install additional packages
 read -p "Do you want to install additional packages? (y/n): " install_extra
